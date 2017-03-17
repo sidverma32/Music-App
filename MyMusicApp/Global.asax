051343b7c448d9344b087deabdd5427fbe4d27f3
sidverma <sid.verma32@gmail.com>
2017-03-18 01:55:53 +0530

    configuration and data modelling files

---
+++ MyMusicApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyMusicPlayer.MvcApplication" Language="C#" %>
---
+++ MyMusicApp/MusicPlayerMaster.cs
@@ -0,0 +1,16 @@
+
+
+namespace MyMusicPlayerMVC
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class MusicPlayerMaster
+    {
+        public int MusicID { get; set; }
+        public string SingerName { get; set; }
+        public string AlbumName { get; set; }
+        public string MusicFileName { get; set; }
+        public string Description { get; set; }
+    }
+}
